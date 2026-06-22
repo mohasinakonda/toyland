7e6c3082e28bdc05e1e584580861f63c1e1c8e51
mohasinakonda <mohasinakonda39@gmail.com>
2026-06-22 13:36:46 +0600

    chore: update build scripts to include prisma generation and clean up prisma client imports

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import Database from "better-sqlite3";
 import path from "path";
 
 declare global {
